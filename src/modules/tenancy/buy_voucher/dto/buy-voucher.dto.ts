1fdc456b00642ea3987e8ad3bc1b0fdd33406b62
Tanvir Ahmed <mdtanvirahmedakash.1@gmail.com>
2026-03-26 03:19:47 +0600

    chanegs buy-voucher.dto

--- src/modules/tenancy/buy_voucher/dto/buy-voucher.dto.ts
+++ src/modules/tenancy/buy_voucher/dto/buy-voucher.dto.ts
@@ -25,11 +25,16 @@
 //   @IsEnum(PaymentMethod)
 //   paymentMethod: PaymentMethod;
 // }
-import { IsString, IsEmail, IsOptional, IsEnum } from 'class-validator';
-import { Prisma } from '@prisma/client';
 
-// Use string union from Prisma
-type PaymentMethod = Prisma.PaymentMethod;
+import { IsString, IsEmail, IsOptional, IsEnum } from 'class-validator';
+
+// Direct string union matching Prisma schema
+export type PaymentMethod =
+  | 'PAY_IN_SALON'
+  | 'CARD'
+  | 'APPLE_PAY'
+  | 'GOOGLE_PAY'
+  | 'BANK_TRANSFER';
 
 export class BuyVoucherDto {
   @IsString()
